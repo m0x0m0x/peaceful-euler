fd343861875b39c4daaeec073deb68ad783044f2
m0x0m0x <123001669+m0x0m0x@users.noreply.github.com>
2024-03-06 09:08:50 +0000

    fix: Fixed typo in comments  file

--- wjs/s8.js
+++ wjs/s8.js
@@ -10,3 +10,15 @@ let lastName = 'Williams';
 let oldLastName = lastName;
 lastName = 'Davis';
 console.log(lastName, oldLastName);
+
+// Testing wiht objects 
+
+const jessica = {
+  firstName: 'Jessica',
+  lastName: 'Williams',
+  age: 27
+};
+const marriedJessica = jessica;
+marriedJessica.lastName = 'Davis';
+console.log('Before marriage:', jessica);
+console.log('After marriage:', marriedJessica);
