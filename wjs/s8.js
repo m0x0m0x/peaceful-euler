b531c05391a9f2ff15df401ea4d1d18bbdc87e76
m0x0m0x <123001669+m0x0m0x@users.noreply.github.com>
2024-03-06 08:44:50 +0000

    feat: Add new path 'wjs' and update file paths, add Section 8 content and add new styles for fonts, body, nav, main, headings, and paragraphs

---
+++ wjs/s8.js
@@ -0,0 +1,7 @@
+console.log(` %c
+=======================================
+100: Primitives vs Objects in Practice
+=======================================
+`, 'color:red;font-size:20px;')
+
+// Section has stuff from the tutorial
